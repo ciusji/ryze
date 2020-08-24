54be0cc988edb16929cac4eb113d27b6e5996b1f
cius.ji <bqjimaster@gmail.com>
2020-08-24 15:55:08 +0800

    feat: fix error page sytle

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Grid, Paper, Typography, Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import classNames from "classnames";
+import { Grid, Typography} from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
+
 
 // styles
 import useStyles from "./styles";
@@ -11,41 +11,13 @@ export default function Error() {
 
   return (
     <Grid container className={classes.container}>
-      <div className={classes.logotype}>
-        <Typography variant="h3" color="white" className={classes.logotypeText}>
-          Ryze
-        </Typography>
-      </div>
-      <Paper classes={{ root: classes.paperRoot }}>
-        <Typography
-          variant="h1"
-          color="primary"
-          className={classNames(classes.textRow, classes.errorCode)}
-        >
-          404
-        </Typography>
-        <Typography variant="h5" color="primary" className={classes.textRow}>
-          Oops. Looks like the page you're looking for no longer exists
-        </Typography>
-        <Typography
-          variant="h6"
-          color="text"
-          colorBrightness="secondary"
-          className={classNames(classes.textRow, classes.safetyText)}
-        >
-          But we're here to bring you back to safety
-        </Typography>
-        <Button
-          variant="outlined"
-          color="primary"
-          component={Link}
-          to="/"
-          size="large"
-          className={classes.backButton}
-        >
-          Back to Home
-        </Button>
-      </Paper>
+      <Typography variant="h3" className={classes.logoText}>
+        404
+      </Typography>
+      <Divider orientation="vertical" variant="middle" className={classes.divider} />
+      <Typography variant="h6" className={classes.errorText}>
+        This page could not be found.
+      </Typography>
     </Grid>
   );
 }
--- src/pages/error/styles.js
+++ src/pages/error/styles.js
@@ -5,57 +5,23 @@ export default makeStyles(theme => ({
     height: "100vh",
     width: "100vw",
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "row",
     justifyContent: "center",
+    backgroundColor: "white",
     alignItems: "center",
-    backgroundColor: theme.palette.primary.main,
     position: "absolute",
     top: 0,
     left: 0,
   },
-  logotype: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: theme.spacing(12),
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  logotypeText: {
-    fontWeight: 500,
-    color: "white",
-    marginLeft: theme.spacing(2),
-  },
-  logotypeIcon: {
-    width: 70,
-    marginRight: theme.spacing(2),
-  },
-  paperRoot: {
-    boxShadow: theme.customShadows.widgetDark,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-    paddingLeft: theme.spacing(6),
-    paddingRight: theme.spacing(6),
-    maxWidth: 404,
-  },
-  textRow: {
-    marginBottom: theme.spacing(10),
-    textAlign: "center",
-  },
-  errorCode: {
-    fontSize: 148,
+  logoText: {
     fontWeight: 600,
+    color: theme.palette.primary,
   },
-  safetyText: {
+  divider : {
+    height: 32,
+  },
+  errorText: {
     fontWeight: 300,
-    color: theme.palette.text.hint,
-  },
-  backButton: {
-    boxShadow: theme.customShadows.widget,
-    textTransform: "none",
-    fontSize: 22,
+    color: theme.palette.secondary,
   },
 }));
